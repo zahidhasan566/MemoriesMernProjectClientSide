ef39021113871df39927a93714974f285abbfaff
Zahid_Hasan143 <zahidh.daraz@gmail.com>
2022-07-27 15:19:54 +0600

    v-1

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React,{useEffect, useState} from 'react';
+import './App.css';
+import { Container, AppBar, Typography , Grow , Grid} from '@material-ui/core';
+import memories from './images/memories.png';
+import Posts from './componenets/Posts/Posts.js';
+import Form from './componenets/Form/Form.js';
+import useStyles from './styles';
+import { useDispatch } from 'react-redux';
+import { getPosts } from './actions/posts';
+const App= ()=> {
+
+  const classes = useStyles();
+  const dispatch = useDispatch();
+   const [currentId,setCurrentId]= useState(null);
+  useEffect (()=>{
+      dispatch(getPosts());
+  },[currentId,dispatch]);
+
+  return (
+     <Container maxWidth= "lg">
+      <AppBar className= {classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center"> memories</Typography>
+        <img className={classes.image} src = {memories} alt="memories" height="60"/>
+      </AppBar>
+      <Grow in>
+        <Container>
+        <Grid container justifyContent="space-between" alignItems="stretch" spacing= {3}>
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId= {setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId}/>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+     </Container>
+  );
+}
+
+export default App;
---
+++ src/reducers/posts.js
@@ -0,0 +1,23 @@
+export default (posts = [], action) => {
+    switch (action.type){
+        case 'FETCH_ALL':
+            return action.payload;
+        break;
+        case 'CREATE':
+            return [...posts, action.payload];
+        break;
+        case 'UPDATE':
+        case 'LIKE':    
+            return posts.map((post)=> post._id == action.payload._id ? action.payload : post);
+        break;
+        case 'DELETE':
+            return posts.filter((post)=> post._id != action.payload);
+        break;
+        default:
+            return posts;
+        
+    }
+    // if(action.type == 'CREATE'){
+
+    // }
+}
